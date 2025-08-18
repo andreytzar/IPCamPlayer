39a352e764960485d4486f9200e0a9d1b35e2a6f
admyndc@gmail.com <admyndc@gmail.com>
2025-08-18 12:52:42 +0300

    Save

---
+++ Helpers/VM/BCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace IPCamPlayer.Helpers.VM
+{
+    public class BCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public BCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+        public BCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) =>
+            _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _execute?.Invoke(parameter);
+        public void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
+    }
+}
---
+++ Helpers/VM/VMNotifyPropretyChanged.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IPCamPlayer.Helpers.VM
+{
+    public class VMNotifyPropretyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
