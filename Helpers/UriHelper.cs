157bc692a25da5c81c6e5f3691318ecd3724a029
admyndc@gmail.com <admyndc@gmail.com>
2025-08-21 18:27:01 +0300

    Save

---
+++ Helpers/UriHelper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace IPCamPlayer.Helpers
+{
+    public static class UriHelper
+    {
+        public static bool IsValidAbsoluteUri(string uriString)
+        {
+            return Uri.TryCreate(uriString, UriKind.Absolute, out var uri) && uri.IsAbsoluteUri;
+        }
+
+        public static Uri? TryCreateUri(string uriString, UriKind kind = UriKind.Absolute)
+        =>Uri.TryCreate(uriString, kind, out var uri) ? uri : null;
+        
+        public static bool IsHttpOrHttps(string uriString)
+        {
+            if (Uri.TryCreate(uriString, UriKind.Absolute, out var uri))
+                return uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps;
+            return false;
+        }
+    }
+}
--- Pages/PageOnVif.xaml.cs
+++ Pages/PageOnVif.xaml.cs
@@ -1,4 +1,6 @@
-﻿using IPCamPlayer.Helpers.VM;
+﻿using IPCamPlayer.Classes;
+using IPCamPlayer.Helpers.VM;
+using System.Security;
 using System.Windows.Controls;
 
 namespace IPCamPlayer.Pages
@@ -13,13 +15,42 @@ namespace IPCamPlayer.Pages
             DataContext = vm = new();
             StatusChanged += statusChanged;
         }
+
+        private void Connect_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            SecureString securePassword = passBox.SecurePassword;
+            vm.Connect(securePassword);
+        }
     }
     public class VMPageOnVif: VMNotifyPropretyChanged
     {
+        string _Login = string.Empty;
+        public string Login { get => _Login; set { if (_Login != value) { _Login = value; OnPropertyChanged(); } } }
+        string _Url = "http://10.254.158.2:90/";
+        public string Url { get => _Url; set { if (_Url != value) { _Url = value; OnPropertyChanged(); } } }
+
+        string _StatusOnvif = string.Empty;
+        public string StatusOnvif { get => _StatusOnvif; set { if (_StatusOnvif != value) { _StatusOnvif = value; OnPropertyChanged(); } } }
+        
         internal event EventHandler<string>? statusChanged;
+        OnVifDev _device = new();
         public VMPageOnVif()
         {
-
+            _device.Status += _device_Status;
         }
+
+
+        public void Connect(SecureString pass) => Task.Run(() => ConnectAsync(pass));
+
+
+        async Task ConnectAsync(SecureString pass)
+        {
+            StatusOnvif=string.Empty;
+            var res = await _device.Connect(Url, pass,Login);
+        }
+
+        private void _device_Status(object? sender, string e)
+        =>StatusOnvif = $"{StatusOnvif}{DateTime.Now.ToString("HH:mm:ss")} {e}\r";
+        
     }
 }
